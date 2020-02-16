430e9e90e709f9cbdacb562f0134cc4bfaeb7118
Ahmad <ahmedmageed19@gmail.com>
2020-02-16 22:56:49 +0200

    Added .. Buttons + graph + simple layout

--- Task1/plotwidg.py
+++ Task1/plotwidg.py
@@ -1,69 +1,98 @@
-from PyQt5 import QtWidgets, uic, QtCore
+from PyQt5 import QtWidgets, uic, QtCore, QtGui
 import pyqtgraph as pg
-import  startstop as ss
+import  startstop3 as ss
 import pandas as pd
 import sys
+from pyqtgraph import PlotWidget
 
 
 class signalViewer(ss.Ui_MainWindow):
     i = 0 # counter represents the chunks size of data to be loaded
-    def __init__(self, mainwindow):
+    channel = 1
+    def __init__(self, mainwindow, speed):
         '''
         Main loop of the UI
         :param mainwindow: QMainWindow Object
         '''
         super(signalViewer, self).setupUi(mainwindow)
-        self.load_data()
+        # self.data = {}
+        self.speed = speed
+        # self.load_bt.clicked.connect(self.load)
+        self.load_data('samples3.csv')
         self.plot()
         self.timer()
+        # print(self.filename)
+        # print(self.format)
 
-    def load_data(self):
+    # def load(self):
+    #     self.filename , self.format= QtWidgets.QFileDialog.getOpenFileName(None, 'Load Signal','/home', "Data Files (*.csv)")
+
+    def load_data(self, filename):
         '''
         load the data from user
         :return:
         '''
-        self.data = pd.read_csv('samples3.csv').astype(float)
-        self.y = self.data.iloc[:200, 1]
+
+        self.data = pd.read_csv(filename)
+        self.y = self.data.iloc[:1, 1]
+        self.x = self.data.iloc[:1, 2] # TODO : Convert to dictionary holding all channels
 
     def plot(self):
         '''
         main plotting function
         :return:
         '''
-        self.widget.setBackground('w')
-        pen = pg.mkPen(color=(255, 0, 0))
-        self.data_line = self.widget.plot(self.y, pen=pen)
+        # TODO : Adapt to plot all channels in channels dict
+
+        # Color of each line
+        pen2 = pg.mkPen(color=(255, 0, 0))
+        pen1 = pg.mkPen(color=(0, 255, 0))
+        # Setting ranges of the x and y axis
+        self.widget.setYRange(min=-1, max=1)
+        self.widget.setXRange(min=0, max=4000)
+        # set title and add legend
+        self.widget.plotItem.setTitle("PLOT")
+        self.widget.plotItem.addLegend(size=(2,3))
+        # Grid
+        self.widget.plotItem.showGrid(True, True, alpha=0.4)
+        self.widget.plotItem.setLabel('bottom', text='Time (ms)')
+
+        #Ploting
+        self.data_line1 = self.widget.plotItem.plot(self.y, pen=pen1, name='y')
+        self.data_line2 = self.widget.plotItem.plot(self.x, pen=pen2, name= 'x')
+
 
     def update_plot_data(self):
         '''
         update function .... add chunks to self.y from loaded data self.data
         :return:
         '''
-        signalViewer.i += 200
-        self.y = pd.concat([self.y, self.data.iloc[self.i:self.i+200, 1]], axis=0, sort=True)
-        self.data_line.setData(self.y)
+        signalViewer.i += 20
+        self.y = pd.concat([self.y, self.data.iloc[signalViewer.i:signalViewer.i+self.speed, 1]], axis=0, sort=True)
+        self.data_line1.setData(self.y)
+        self.x = pd.concat([self.x, self.data.iloc[signalViewer.i:signalViewer.i+self.speed, 2]], axis=0, sort=True)
+        self.data_line2.setData(self.x)
 
     def timer(self):
         self.timer = QtCore.QTimer()
         self.timer.setInterval(50)
         self.timer.timeout.connect(self.update_plot_data)
-        # self.timer.start() ### turn to Button
         self.start_timer()
-        self.stop_timer()
+        self.pause_timer()
 
     def start_timer(self):
         '''
         Event of clicking the start button which starts the signal plotting
         :return:
         '''
-        self.toolButton.clicked.connect(self.timer.start)
+        self.start_bt.clicked.connect(self.timer.start)
 
-    def stop_timer(self):
+    def pause_timer(self):
         '''
         Event of clicking the stop button which stops the signal plotting
         :return:
         '''
-        self.toolButton_2.clicked.connect(self.timer.stop)
+        self.pause_bt.clicked.connect(self.timer.stop)
 
 
 
@@ -74,7 +103,7 @@ def main():
     '''
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = signalViewer(MainWindow)
+    ui = signalViewer(MainWindow, speed=50)
     MainWindow.show()
     sys.exit(app.exec_())
 
