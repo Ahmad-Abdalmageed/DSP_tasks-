57e23779690eed961b5a4e7d747fef2aafe415f7
Abdullah Alrefaey <abdullah.m.alrefaey@gmail.com>
2020-02-17 07:02:39 +0200

    Added plot functions for csv, mat, txt

--- Task1/plotwidg.py
+++ Task1/plotwidg.py
@@ -1,7 +1,8 @@
 from PyQt5 import QtWidgets, uic, QtCore, QtGui
 import pyqtgraph as pg
-import  startstop3 as ss
+import startstop3 as ss
 import pandas as pd
+from scipy.io import loadmat
 import sys
 from pyqtgraph import PlotWidget
 
@@ -17,25 +18,65 @@ class signalViewer(ss.Ui_MainWindow):
         super(signalViewer, self).setupUi(mainwindow)
         # self.data = {}
         self.speed = speed
-        # self.load_bt.clicked.connect(self.load)
-        self.load_data('samples3.csv')
+
+        # Load Button connector
+        self.load_bt.clicked.connect(self.load)
+
+        # Plot Configurations
+
+        # Pen color
+        self.pen1 = pg.mkPen(color=(0, 255, 0))
+
+        # Setting ranges of the x and y axis
+        self.widget.setYRange(min=-1, max=1)
+        self.widget.setXRange(min=0, max=4000)
+
+        # set title and add legend
+        self.widget.plotItem.setTitle("PLOT")
+        self.widget.plotItem.addLegend(size=(2, 3))
+
+        # Grid
+        self.widget.plotItem.showGrid(True, True, alpha=0.8)
+        self.widget.plotItem.setLabel('bottom', text='Time (ms)')
+
+    def load(self):
+        self.filename , self.format= QtWidgets.QFileDialog.getOpenFileName(None, 'Load Signal','/home', "Data Files (*)")
+        self.signal_file = self.filename.split("/")[-1]
+        self.checkFileExt(self.signal_file)
+
+    def plotSignal_csv(self, filename):
+        self.load_csv_data(filename)
         self.plot()
         self.timer()
-        # print(self.filename)
-        # print(self.format)
 
-    # def load(self):
-    #     self.filename , self.format= QtWidgets.QFileDialog.getOpenFileName(None, 'Load Signal','/home', "Data Files (*.csv)")
+    def plotSignal_mat(self, filename):
+        self.load_mat_data(filename)
+        self.plot()
+        self.timer()
 
-    def load_data(self, filename):
+    def plotSignal_txt(self, filename):
+        self.load_txt_data(filename)
+        self.plot()
+        self.timer()
+
+    def load_csv_data(self, file_name):
         '''
         load the data from user
         :return:
         '''
 
-        self.data = pd.read_csv(filename)
+        self.data = pd.read_csv(file_name)
         self.y = self.data.iloc[:1, 1]
-        self.x = self.data.iloc[:1, 2] # TODO : Convert to dictionary holding all channels
+        # self.x = self.data.iloc[:1, 2] # TODO : Convert to dictionary holding all channels
+
+    def load_mat_data(self, file_name):
+        mat_file = loadmat(file_name)
+        self.data = pd.DataFrame(mat_file['F'])
+        self.y = self.data.iloc[:1, 1]
+
+    def load_txt_data(self, file_name):
+        self.data = pd.read_csv(file_name, skiprows=[i for i in range(500,7657)])
+        self.y = self.data.iloc[:1, 2]
 
     def plot(self):
         '''
@@ -44,22 +85,8 @@ class signalViewer(ss.Ui_MainWindow):
         '''
         # TODO : Adapt to plot all channels in channels dict
 
-        # Color of each line
-        pen2 = pg.mkPen(color=(255, 0, 0))
-        pen1 = pg.mkPen(color=(0, 255, 0))
-        # Setting ranges of the x and y axis
-        self.widget.setYRange(min=-1, max=1)
-        self.widget.setXRange(min=0, max=4000)
-        # set title and add legend
-        self.widget.plotItem.setTitle("PLOT")
-        self.widget.plotItem.addLegend(size=(2,3))
-        # Grid
-        self.widget.plotItem.showGrid(True, True, alpha=0.4)
-        self.widget.plotItem.setLabel('bottom', text='Time (ms)')
-
-        #Ploting
-        self.data_line1 = self.widget.plotItem.plot(self.y, pen=pen1, name='y')
-        self.data_line2 = self.widget.plotItem.plot(self.x, pen=pen2, name= 'x')
+        self.data_line1 = self.widget.plotItem.plot(self.y, pen=self.pen1, name='y')
+        # self.data_line2 = self.widget.plotItem.plot(self.x, pen=pen2, name= 'x')
 
 
     def update_plot_data(self):
@@ -67,11 +94,13 @@ class signalViewer(ss.Ui_MainWindow):
         update function .... add chunks to self.y from loaded data self.data
         :return:
         '''
-        signalViewer.i += 20
+
+        signalViewer.i += 30
         self.y = pd.concat([self.y, self.data.iloc[signalViewer.i:signalViewer.i+self.speed, 1]], axis=0, sort=True)
         self.data_line1.setData(self.y)
-        self.x = pd.concat([self.x, self.data.iloc[signalViewer.i:signalViewer.i+self.speed, 2]], axis=0, sort=True)
-        self.data_line2.setData(self.x)
+        # self.x = pd.concat([self.x, self.data.iloc[signalViewer.i:signalViewer.i+self.speed, 2]], axis=0, sort=True)
+        # self.data_line2.setData(self.x)
+
 
     def timer(self):
         self.timer = QtCore.QTimer()
@@ -94,6 +123,13 @@ class signalViewer(ss.Ui_MainWindow):
         '''
         self.pause_bt.clicked.connect(self.timer.stop)
 
+    def checkFileExt(self, file):
+        if file.endswith('.csv'):
+            self.plotSignal_csv(file)
+        elif file.endswith('.txt'):
+            self.plotSignal_txt(file)
+        elif file.endswith('.mat'):
+            self.plotSignal_mat(file)
 
 
 def main():
